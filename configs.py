81ae8911a549c503ff20608ba9874f91fe47eca0
kaydenx1 <119522615+kaydenx1@users.noreply.github.com>
2022-12-02 18:20:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -21,5 +21,5 @@ class Config(object):
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://gomunomi:gomunomi@cluster0.i0wygey.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001686131036"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
